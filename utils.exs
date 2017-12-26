08d79c1685d6da1de789e4c46beacce311c86f9f
Pablo Gorostiaga <pg1816@ic.ac.uk>
2017-12-26 18:51:12 +0100

    added @spec to utils

--- utils.exs
+++ utils.exs
@@ -1,10 +1,12 @@
 defmodule Utils do
-
+  
+  @spec allSame(list()) :: boolean()
   def allSame([]), do: true
   def allSame(list = [x | _xs]) do
     list |> Enum.map(fn(y) -> x == y end) |> Enum.all?
   end
 
+  @spec remove(term(), list()) :: list()
   def remove(_, []), do: []
   def remove(x, ps) do
     for p <- ps,
@@ -12,11 +14,13 @@ defmodule Utils do
       do: p
   end
 
+  @spec lookUp(term(), list({term(), term()})) :: {term(), term()}
   def lookUp(x, pairList) do
     # returns the first one, assumes there is always one match
     pairList |> Enum.filter(fn(pair) -> x == fst pair end) |> hd
   end
 
+  @spec xlogx(number()) :: number()
   def xlogx(d) do
     cond do
       d <= 1.0e-100 -> 0.0
@@ -24,6 +28,9 @@ defmodule Utils do
     end
   end
 
+  @spec snd({term(), term()}) :: term()
   def snd(pair), do: elem(pair, 1)
+
+  @spec fst({term(), term()}) :: term()
   def fst(pair), do: elem(pair, 0)
 end
